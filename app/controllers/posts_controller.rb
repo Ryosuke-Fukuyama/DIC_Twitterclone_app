d982019808c00e504911422d7a4c7012da3f633c
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-04-18 22:13:01 +0900

    アクション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,46 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
-
   def new
+    @post = Post.new
   end
-
-  def edit
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
   end
-
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "作成しました！"
+      else
+        render :new
+      end
+    end
+  end
   def show
   end
-
-  def confirm
+  def edit
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました！"
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
