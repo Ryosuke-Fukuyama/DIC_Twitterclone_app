6b4bea31dc2060327b4b98e2f5fbbe8e6db249ab
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-04-18 21:48:32 +0900

    index実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/show'
-  get 'posts/confirm'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
 end
