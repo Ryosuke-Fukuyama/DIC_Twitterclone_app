5ff9f4a58700cc70af7a0c7a9743959d717b2586
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-04-18 20:52:25 +0900

    helperとassetsを作成しない設定

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,11 @@ Bundler.require(*Rails.groups)
 module TClone
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
